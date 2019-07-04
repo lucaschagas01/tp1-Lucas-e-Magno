b37a752a0ad010b80c78812ac3f1c12581d65f05
lucaschagas01 <lucaschagasbr01@gmail.com>
2019-07-04 10:02:43 -0300

    Cliente

---
+++ gatojoava/src/gatojoava/kabum/Cliente.java
@@ -0,0 +1,94 @@
+
+public class java {
+   private String nome;
+   private int cpf;
+   private String email;
+   private int senha;
+   private String idade;
+   private int rg;
+
+    /**
+     * @return the nome
+     */
+    public String getNome() {
+        return nome;
+    }
+
+    /**
+     * @param nome the nome to set
+     */
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    /**
+     * @return the cpf
+     */
+    public int getCpf() {
+        return cpf;
+    }
+
+    /**
+     * @param cpf the cpf to set
+     */
+    public void setCpf(int cpf) {
+        this.cpf = cpf;
+    }
+
+    /**
+     * @return the email
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * @param email the email to set
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    /**
+     * @return the senha
+     */
+    public int getSenha() {
+        return senha;
+    }
+
+    /**
+     * @param senha the senha to set
+     */
+    public void setSenha(int senha) {
+        this.senha = senha;
+    }
+
+    /**
+     * @return the idade
+     */
+    public String getIdade() {
+        return idade;
+    }
+
+    /**
+     * @param idade the idade to set
+     */
+    public void setIdade(String idade) {
+        this.idade = idade;
+    }
+
+    /**
+     * @return the rg
+     */
+    public int getRg() {
+        return rg;
+    }
+
+    /**
+     * @param rg the rg to set
+     */
+    public void setRg(int rg) {
+        this.rg = rg;
+    }
+}
+
